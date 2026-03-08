1135e19e37095dec75d33faddd8f04437c46a1d5
TaTiaNaM <maluchenko.tania@gmail.com>
2026-03-08 15:36:03 +0200

    terminal

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,12 +1,12 @@
 import { useState, useEffect } from 'react'
 import './App.module.css'
 import { Movie } from '../types/movie'
-// import axios from 'axios'
+import axios from 'axios'
 import SearchBar from '../SearchBar/SearchBar';
 
 export default function App({id, title}: Movie) {
     const handleSearch = (query: string) => {
-        console.log('Search query:', query);
+        axios.get<Movie[]>(`https://image.tmdb.org/t/p/w500${query}`)
     };
 
 //     const poster_path = '/kqjL17yufvn9OVLyXYpvtyrFfak.jpg';
